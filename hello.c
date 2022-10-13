a9ea3b3cf672c8fb6d2e9a079b7cbb6e88836136
Cecyad <cecy.95.ad@gmail.com>
2022-10-13 09:26:10 -0600

    New changes

--- hello.c
+++ hello.c
@@ -3,10 +3,7 @@
 
 int main(void) {
     printf("Hello world!\n");
-<<<<<<< HEAD
     printf("This is a second line\n");
-=======
     printf("The double of 5 is %i",doubleInt(5));
->>>>>>> math
     return 0;
 }
